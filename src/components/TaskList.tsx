0f609020862669594395ffb81b14d95fa8338070
Maykon A. Oliveira <maykon.oliveira@neoway.com.br>
2021-03-09 17:40:03 -0300

    pass the tests

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,15 +15,35 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (newTaskTitle !== '') {
+      setTasks([ ...tasks, {
+        id: Math.random(),
+        title: newTaskTitle,
+        isComplete: false
+      }])
+    }
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const taskIndex = tasks.findIndex((value) => {
+      return value.id === id;
+    })
+
+    const updatedTasks = [ ...tasks ]
+    updatedTasks[taskIndex].isComplete = !updatedTasks[taskIndex].isComplete
+
+    setTasks(updatedTasks)
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    const taskIndex = tasks.findIndex((value) => {
+      return value.id === id;
+    })
+    
+    const updatedTasks = [ ...tasks ]
+    updatedTasks.splice(taskIndex, 1)
+
+    setTasks(updatedTasks)
   }
 
   return (
